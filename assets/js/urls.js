9595e32cd91d31884f3c94cb94fe1513565634b2
Brian Hie <blhie1@gmail.com>
2024-10-22 15:39:13 -0700

    update urls

--- assets/js/urls.js
+++ assets/js/urls.js
@@ -1,7 +1,7 @@
-const goUrl = 'https://gist.githubusercontent.com/brianhie/eeb2a5fe5f4b8a5651441ac81ef9adf7/raw/77b819b0e0dcad44375aa79d6293a9dc745b07aa/syngenome_test_go.json';
+const goUrl = 'https://gist.githubusercontent.com/brianhie/eeb2a5fe5f4b8a5651441ac81ef9adf7/raw/0c74609df5fd0d39e4bba8283c44a981d6261674/syngenome_test_go.json';
 
-const domainUrl = 'https://gist.githubusercontent.com/brianhie/68b04785817b4da370ba819ac3a9028f/raw/dbfbbb80dc3736eb8017857ba4ce41f2b7ae5514/syngenome_test_domain.json';
+const domainUrl = 'https://gist.githubusercontent.com/brianhie/68b04785817b4da370ba819ac3a9028f/raw/d3185a3a5a995ccfaed7a77084e96b521e98fd0d/syngenome_test_domain.json';
 
-const speciesUrl = 'https://gist.githubusercontent.com/brianhie/b580bf2dcd7223a88cd8f3f4cde0c7f9/raw/e0cfa9022b937215f1d477310a66fdb6afd2bb03/syngenome_test_species.json';
+const speciesUrl = 'https://gist.githubusercontent.com/brianhie/b580bf2dcd7223a88cd8f3f4cde0c7f9/raw/7bf5afc6534f0b702491cae01434d9c88dc7452b/syngenome_test_species.json';
 
 const uniprotUrl = 'https://gist.githubusercontent.com/brianhie/c25b53d61b5fac1e3e2f417401856fb7/raw/de115598d262e1ce477f35b252763840454a1f40/syngenome_test_uniprot.json';
