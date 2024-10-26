faa51019eb5b505a21df1a16f311b2bd9588c0cb
Brian Hie <blhie1@gmail.com>
2024-10-26 12:48:59 -0700

    change url

--- assets/js/urls.js
+++ assets/js/urls.js
@@ -1,7 +1,7 @@
-const goUrl = 'https://gist.githubusercontent.com/brianhie/eeb2a5fe5f4b8a5651441ac81ef9adf7/raw/0c74609df5fd0d39e4bba8283c44a981d6261674/syngenome_test_go.json';
+const goUrl = 'https://evodesign.org/data/go_records.json.gz'
 
-const domainUrl = 'https://gist.githubusercontent.com/brianhie/68b04785817b4da370ba819ac3a9028f/raw/d3185a3a5a995ccfaed7a77084e96b521e98fd0d/syngenome_test_domain.json';
+const domainUrl = 'https://evodesign.org/data/domain_records.json.gz'
 
-const speciesUrl = 'https://gist.githubusercontent.com/brianhie/b580bf2dcd7223a88cd8f3f4cde0c7f9/raw/7bf5afc6534f0b702491cae01434d9c88dc7452b/syngenome_test_species.json';
+const speciesUrl = 'https://evodesign.org/data/species_records.json.gz'
 
-const uniprotUrl = 'https://evodesign.org/uniprot_records_min.json.gz';
+const uniprotUrl = 'https://evodesign.org/data/uniprot_records.json.gz';
